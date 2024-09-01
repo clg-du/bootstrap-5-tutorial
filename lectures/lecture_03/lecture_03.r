23ac64366f4747cba070b5fb14299e0106091de5
Charlie Lindgren <clg@du.se>
2024-09-01 17:30:23 +0200

    Update lecture materials for Bootstrap buttons

---
+++ lectures/lecture_03/lecture_03.r
@@ -0,0 +1,38 @@
+#' # Lecture 2: Typography and Colors
+#' `r Sys.Date()`
+#+ echo = FALSE
+htmltools::includeHTML("../../index_lectures.html")
+htmltools::includeHTML("../../index_exercises.html")
+#'
+#' ## Overview
+#' 
+#' In this lecture, we will cover the basics of typography and colors in
+#' Bootstrap 5. We will explore the different typography utilities and color
+#' classes provided by Bootstrap. We will also discuss how to use these
+#' utilities to style text and backgrounds in your web pages.
+#'
+#' ## Topics Covered
+#'
+#' - Typography utilities in Bootstrap 5
+#' - Color classes in Bootstrap 5
+#' - Styling text and backgrounds
+#'
+#' ## Resources
+#'
+#' - [Bootstrap typography](https://getbootstrap.com/docs/5.0/content/typography/)
+#' - [Bootstrap headings](https://getbootstrap.com/docs/5.0/content/typography/#headings)
+#' - [Bootstrap display headings](https://getbootstrap.com/docs/5.0/content/typography/#display-headings)
+#' - [Bootstrap lead](https://getbootstrap.com/docs/5.0/content/typography/#lead)
+#' - [Bootstrap inline text elements](https://getbootstrap.com/docs/5.0/content/typography/#inline-text-elements)
+#' - [Bootstrap text colors](https://getbootstrap.com/docs/5.0/utilities/colors/#colors)
+#' - [Bootstrap background colors](https://getbootstrap.com/docs/5.0/utilities/colors/#background-color)
+#' - [Emmet Abbreviation Lorem Ipsum generation](https://docs.emmet.io/abbreviations/lorem-ipsum/)
+#'
+#' ## Instructions
+#'
+#' 1. Open the "index.html" file in the lecture directory.
+#' 1. Browse through the different typography utilities and color classes.
+#'
+#+ echo = FALSE
+htmltools::includeHTML("../../index_lectures.html")
+htmltools::includeHTML("../../index_exercises.html")
