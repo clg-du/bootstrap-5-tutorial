95c6e034f8408bf456f522ee644cea4f13b3a2ac
Charlie Lindgren <clg@du.se>
2024-09-01 17:39:50 +0200

    Update lecture materials for Bootstrap buttons

--- lectures/lecture_03/lecture_03.r
+++ lectures/lecture_03/lecture_03.r
@@ -1,37 +1,38 @@
-#' # Lecture 2: Typography and Colors
+#' # Lecture 3: Buttons
 #' `r Sys.Date()`
 #+ echo = FALSE
 htmltools::includeHTML("../../index_lectures.html")
 htmltools::includeHTML("../../index_exercises.html")
 #'
 #' ## Overview
-#' 
-#' In this lecture, we will cover the basics of typography and colors in
-#' Bootstrap 5. We will explore the different typography utilities and color
-#' classes provided by Bootstrap. We will also discuss how to use these
-#' utilities to style text and backgrounds in your web pages.
+#'
+#' In this lecture, we will cover the basics of buttons in Bootstrap 5. Buttons
+#' are an essential component of web design and are used to trigger actions,
+#' such as submitting forms, navigating to different pages, or performing other
+#' interactive tasks. We will explore the different button styles, sizes, and
+#' states provided by Bootstrap and learn how to create attractive and
+#' interactive buttons for your web pages.
 #'
 #' ## Topics Covered
 #'
-#' - Typography utilities in Bootstrap 5
-#' - Color classes in Bootstrap 5
-#' - Styling text and backgrounds
+#' - Basic buttons
+#' - Anchor tag buttons
+#' - Button sizes
+#' - Button styles
+#' - Grouped buttons
 #'
 #' ## Resources
 #'
-#' - [Bootstrap typography](https://getbootstrap.com/docs/5.0/content/typography/)
-#' - [Bootstrap headings](https://getbootstrap.com/docs/5.0/content/typography/#headings)
-#' - [Bootstrap display headings](https://getbootstrap.com/docs/5.0/content/typography/#display-headings)
-#' - [Bootstrap lead](https://getbootstrap.com/docs/5.0/content/typography/#lead)
-#' - [Bootstrap inline text elements](https://getbootstrap.com/docs/5.0/content/typography/#inline-text-elements)
-#' - [Bootstrap text colors](https://getbootstrap.com/docs/5.0/utilities/colors/#colors)
-#' - [Bootstrap background colors](https://getbootstrap.com/docs/5.0/utilities/colors/#background-color)
-#' - [Emmet Abbreviation Lorem Ipsum generation](https://docs.emmet.io/abbreviations/lorem-ipsum/)
+#' - [Basic buttons](https://getbootstrap.com/docs/5.0/components/buttons/)
+#' - [Anchor tag buttons](https://getbootstrap.com/docs/5.0/components/buttons/#anchor-tags)
+#' - [Button sizes](https://getbootstrap.com/docs/5.0/components/buttons/#sizing)
+#' - [Button styles](https://getbootstrap.com/docs/5.0/components/buttons/#examples)
+#' - [Grouped buttons](https://getbootstrap.com/docs/5.0/components/button-group/)
 #'
 #' ## Instructions
 #'
 #' 1. Open the "index.html" file in the lecture directory.
-#' 1. Browse through the different typography utilities and color classes.
+#' 1. Browse through the different button styles, sizes, and states.
 #'
 #+ echo = FALSE
 htmltools::includeHTML("../../index_lectures.html")
