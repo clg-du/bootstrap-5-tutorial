7220bb84c4cb7d78b9cd0e3ec6200aa550d14806
Charlie Lindgren <clg@du.se>
2024-09-03 09:28:51 +0200

    chore: Update lecture materials for Bootstrap buttons

---
+++ lectures/lecture_06/lecture_06.r
@@ -0,0 +1,33 @@
+#' # Lecture 6: Grid layout
+#' `r Sys.Date()`
+#+ echo = FALSE
+htmltools::includeHTML("../../index_lectures.html")
+htmltools::includeHTML("../../index_exercises.html")
+#'
+#' ## Overview
+#'
+#' In this lecture, we will learn about the grid layout system in Bootstrap. The
+#' grid layout system is a powerful and flexible system that allows you to
+#' create complex layouts with ease. We will learn about the grid system, grid
+#' classes, and how to create responsive layouts using the grid system.
+#' 
+#' ## Topics Covered
+#'
+#' - Introduction to the grid layout system
+#' - Grid classes
+#' - Grid breakpoints
+#' - Grid alignment
+#' - Grid nesting
+#'
+#' ## Resources
+#'
+#' - [Bootstrap Grid System](https://getbootstrap.com/docs/4.5/layout/grid/)
+#' 
+#' ## Instructions
+#'
+#' 1. Open the "index.html" file in the lecture directory.
+#' 1. Explore the examples of the grid layout system.
+#'
+#+ echo = FALSE
+htmltools::includeHTML("../../index_lectures.html")
+htmltools::includeHTML("../../index_exercises.html")
